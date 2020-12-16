8abf3b2b8880e76f6358511f7d012bca1facd636
AryanGoyal80 <goyalaryan51@gmail.com>
2020-12-16 12:48:11 +0530

    first commit

---
+++ commands/misc/suggest.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+
+module.exports = {
+  commands: ['suggest', 'suggestion', 'poll'],
+  minArgs: 1,
+  expectedArgs: '<!poll text>',
+
+  callback: async (message, arguments, text) => {
+    const pollchannel = message.client.channels.cache.get('788664173713817641');
+    
+      let suggestembed = new Discord.MessageEmbed()
+        .setColor('#337f4e')
+        .setTimestamp()
+        .addFields(
+          { name: `Suggestion:`, value: `${text}` })
+      
+      //send embed and react to it with up/down
+      let reactsuggest = await suggestchannel.send(suggestembed);
+      reactsuggest.react('👍')
+      reactsuggest.react('👎')
+      
+      await pollchannel.send(suggestembed);     
+    
+    }
+
+
+}
