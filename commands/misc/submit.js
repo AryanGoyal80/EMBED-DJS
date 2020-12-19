ff0e27ed0884eb6c12a45a538b6512834ba39998
AryanGoyal80 <goyalaryan51@gmail.com>
2020-12-19 14:37:48 +0530

    first commit

--- commands/misc/submit.js
+++ commands/misc/submit.js
@@ -6,12 +6,13 @@ module.exports = {
     
 const { member, mentions } = message
 const tag = `<@${member.id}>`
-const verified = `${arguments[0]}`
+var person = message.guild.member(message.mentions.users.first());
+const temprole = message.guild.roles.cache.find(role => role.name === "temp")
 
 const discussionchannel = message.client.channels.cache.get('788131164941582338');
 
 await discussionchannel.send(`${arguments[0]} your response has been submitted 🥳🥳. Thank you ${tag} for approving. :smile: `)
-       verified.roles.remove(temprole.id)
+       person.roles.remove(temprole.id)
 }
 
 }
