d4de5ee023ca218e2420f46a5724a5fe65cc1d14
AryanGoyal80 <goyalaryan51@gmail.com>
2020-12-19 14:27:00 +0530

    first commit

--- commands/misc/submit.js
+++ commands/misc/submit.js
@@ -6,9 +6,12 @@ module.exports = {
     
 const { member, mentions } = message
 const tag = `<@${member.id}>`
+const verified = `${arguments[0]}`
 
 const discussionchannel = message.client.channels.cache.get('788131164941582338');
 
 await discussionchannel.send(`${arguments[0]} your response has been submitted 🥳🥳. Thank you ${tag} for approving. :smile: `)
+verified.roles.remove(788763279887171624)
 }
+
 }
