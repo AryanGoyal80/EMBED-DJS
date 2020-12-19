d799ed1d6b926eddfe6cedbd323c1ad91027e90b
AryanGoyal80 <goyalaryan51@gmail.com>
2020-12-19 15:31:50 +0530

    first commit

--- commands/misc/link.js
+++ commands/misc/link.js
@@ -2,8 +2,7 @@ const { minArgs } = require("./submit");
 
 module.exports = {
     commands: ['link', 'links' ],
-    minArgs = 0,
-    maxArgs = 0,
+    minArgs = 1,
     callback: (message, arguments, text) => {
         // here are all the link regarding bot and everything else
         message.channel.send(`Here are all the links: \n\n https://discord.gg/rMsJHGEvDu \n\n  https://drive.google.com/folderview?id=1sBKdnUfx_UFFe1l7mL0CA-cgLgRlEwy3`)
