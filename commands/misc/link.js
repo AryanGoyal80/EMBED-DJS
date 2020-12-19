cd34000c0b38c8941d4377d4a74bc88b71d806c1
AryanGoyal80 <goyalaryan51@gmail.com>
2020-12-19 15:36:40 +0530

    first commit

--- commands/misc/link.js
+++ commands/misc/link.js
@@ -1,7 +1,7 @@
 module.exports = {
-    commands: ['link'],
+    commands: ['link', 'Links', 'Link', 'links'],
     minArgs: 1,
-    callback: async (message, arguments, text) => {
+    callback: (message, arguments, text) => {
         // here are all the link regarding bot and everything else
         message.channel.send(`Here are all the links: \n\n https://discord.gg/rMsJHGEvDu \n\n  https://drive.google.com/folderview?id=1sBKdnUfx_UFFe1l7mL0CA-cgLgRlEwy3`)
     }
