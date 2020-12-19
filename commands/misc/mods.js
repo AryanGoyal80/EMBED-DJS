0a3a6a2ee4280dd15f62ca358ac7ded3b1881025
AryanGoyal80 <goyalaryan51@gmail.com>
2020-12-19 15:08:33 +0530

    first commit

---
+++ commands/misc/mods.js
@@ -0,0 +1,18 @@
+module.exports = {
+    commands: ['promote'],
+    minArgs: 1,
+    permissions: 'ADMINISTRATOR',
+    callback: async (message, arguments, text) => {
+    
+const { member, mentions } = message
+const tag = `<@${member.id}>`
+var roletaking = message.guild.member(message.mentions.users.first());
+const modrole = message.guild.roles.cache.find(role => role.name === "Mods")
+
+const modchannel = message.client.channels.cache.get('788449162919936000');
+
+await modchannel.send(`${arguments[0]} has been alloted mod role :smirk: 🥳🥳 by ${tag} .  `)
+       roletaking.roles.add(modrole.id)
+}
+
+}
