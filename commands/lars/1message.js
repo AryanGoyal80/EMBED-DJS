5c6c8a36d4c83f459ee44b2e9bdba046d9c02f27
AryanGoyal80 <goyalaryan51@gmail.com>
2020-12-28 17:31:54 +0530

    V3

---
+++ commands/lars/1message.js
@@ -0,0 +1,79 @@
+module.exports = {
+    commands: ['betasend1'],
+    minArgs: 0,
+    callback: async (message, arguments, text) => {
+        
+        let BdayEmbed = new Discord.MessageEmbed()
+          .setColor('#ca208b')
+          .setTitle(`**Here are the wishings from the whole sv team** :partying_face:`)
+          .setFooter(`Page 1`)
+          .addFields(
+              { name: '\u200B', value: 'Message:' },
+              { name:  `**From Rafael-** \n Hello Lars, thanks for being such an amazing person and for making the server work so well. I wish you a happy birthday and I hope that all your dreams will come true.:pinched_fingers: :pinched_fingers_tone1: :pinched_fingers_tone2: :pinched_fingers_tone3: :pinched_fingers_tone4: :pinched_fingers_tone5:  \n\n **From Mimi-**  \n Happy birthday my love :birthday: :heart: !
+              I wish you a very nice day :3 You have now been part of my life for 8 month exactly ^^ Thank you for making me a better version of myself everyday, for showing love in all kinds of way, and for being the teasing annoying boiiiii sometimes :stuck_out_tongue:
+              You are the penguin king of this server :penguin: ! (fits well with the snow queen, now that i think of it :thinking: :stuck_out_tongue_winking_eye: ) What you did for our community is amazing :blush:
+              I am really proud of you :sparkling_heart:
+              joyeux anniversaire mon amour :two_hearts:
+              Mimi :flamingo:  \n\n **From Nadir-**  \n Hello My Son Robin ; thanx for being such a cool and awesome friend and mod for making the server always in good vibes for make it look like a peaceful home of our big family everyone of us really appreciate the effort you doing for us Happy birthday dude i really wish the best and success in your life and i hope your dream become true have a great day Lars ................... or Robin lool   \n\n **From Lisa_V-** \n Hiiii, hope you have a wonderful birthday, and that all your wishes may come true :partying_face: let's have a drink on ya bday :beers: :champagne_glass: \n\n **From Aryan-** \n Hello bro. I wish you an amazing bday :partying_face: . I hope you are enjoying your day. Many many happy returns of the day. You are really so friendly, awesome and cool guys here in the server. Thank you for doing so much for us. We all very greatful to you for your such marvelous works. :star_struck: Thanks for helping me every-time whenever I stuck in code :stuck_out_tongue_winking_eye: . You are really so awesome be like this only :smile: And yes enjoy your day don't drink too much :beers: :stuck_out_tongue: . Enjoy your day lars aka penguin :penguin: :partying_face: :partying_face: :tada:  
+              \n\n **From Nandini-** \n Happy birthday Lars! :pancakes: :penguin~2: you're officially 89 and I'm so so happy for you :D Thank you for ALL that you do for our server, we are so grateful for it! You're such an amazing human being, with a very delightful sense of humor and it's just really fun being around you. I wish you the best birthday you can possible have and I hope you have a wonderful year ahead. Good luck for 2021, may you succeed in all your endeavors. Much love :sparkling_heart: - Nandini (you forgot my name the other day so i'm putting it all over my message so that it haunts you in your dreams :stuck_out_tongue:) 
+              \n\n **From Ariadane-** \n Happy Birthday Lars, Hope you have a great day our penguin leader.    
+              \n\n **From Lauren-** \n Happy birthday robin!!! I hope you have an amazing day today! thank you from the bottom of my heart for everything you do for our server. You have been the reason for so many amazing people finding a happy, motivating place on Study Vibes - and no one can thank you enough for that. so once again, i hope today is as brilliant as it should be for you
+              - Laui
+              \n\n **From Simran-** \n Happy Birthday Lars!!!:tada::tada::tada:... Thanks for making this server more efficient day by day. You are the reason that we all are here together and thanx for bringing good vibes around here. Wish you all the success and happiness on you birthday. Hope you have an amazing day!! :champagne_glass: :grin: 
+              \n\n **From Derk-** \n Laaaaaaaaars, my boi, Happy Birthday! Another year older, another year wiser. Altough i don't know if you can get any wiser, because you're already the 'almanak' of the server. Sow, make it another unforgettable day and i'll see you around ; )
+              \n\n  **From Betul-** \n Happy birthday, Robin! :partying_face: I hope your new age comes with luck. Thanks for being with us and giving us best place to study. I hope you reach your dreams and live a happy life. :heart:
+              \n\n **From abhi.kr.2100-**  \n Here is your wishing message
+              Happy 68th birthday, Lars! In just one year from now you'll become a real man. So difficult to believe! Jest aside, in my short history of playing poker, you were the first player to win my respect! And I shouldn't even get started on how important you have been and continue to be on the Study Vibes server!
+              
+              Now, this birthday wish won't have the "Abhishek"-feel without a joke. So here's one especially for you:
+              
+              A German-speaking person and a Dutch-speaking person enter a bar. They both get extremely drunk, and exit the bar as two Dutch-speakers.
+               \n\n **From Zeynpbrfn-** \n happy birthday robinnnnnn :partying_face: i hope you have an amazing day! i can't thank you enough for what you are doing in this server, a lot of people are studying in peace and finding their motivation here and you are responsible for them! and you are such an amazing person, you are kind and you are like dad of this fam. i'm really grateful that i met you :smiling_face_with_3_hearts: i wish yoy the best in your life and please don't forget us :pleading_face: :green_heart:
+               \n\n **From Swazmoh-** \n a very HAPPY BIRTHDAY to the person without whom we wouldnt even be doing this. :partying_face: :birthday: :tada: :confetti_ball: :heart:
+               from the bottom of my heart, thank u for everything that u have done for us and for just being one incredible human being, thank u for all ur efforts man, we are all so grateful towards u. Ur a very hardworking and inspiring for every single person in the server, and i wish u all the best for ur upcoming years!
+               God bless u, stay safe and happy as always, u deserve the very best. :pray:
+               Have a great day and year ahead!
+               Love u man :purple_heart: :yellow_heart:
+               ~Moe :heart:
+               \n\n **From Shaurya-** \n Happy birthday lars :partying_face: :cake:. Thank u so much from the bottom of my heart to do this much for us and always be available for peeps here even if you're busy. You are such a caring and a fun guy to be around. You are really motivating and hardworking person and I wish u all the best for your life ahead. God bless u brother. Take care and stay happy as always :white_heart: .
+               Love u bro :white_heart:
+               - Thomas Shelby :wink: xd
+               \n\n **From Pjay-** \n Happy Birthday Lars!!:100: Hope you have an amazing year ahead and achieve all the things you envision for the next year!:fire: You are one of the funniest and Jovial people i know. You are a true contributor to the betterment of this society by helping thousands of us so far study and be motivated do so by making this server and constantly improving it and making it better suited to our comforts constantly and that for free of cost. I can not fathom how much time,love and passion you have put into this. well i can tell you this: you have my gratitude for life for transforming me into a hard-worker,You get this credit. I still remember asking you why you put so much effort into this for no financial gain. You said one day i will be happy to one day know that i helped create a doctor pilot,scientist,engineer or great person emerges from here. Well,I will strive to be one of those people who reward you this satisfaction. Enjoy your birthday Lars,Just know that you are Pure gold in a majorly selfish world.:hearts:
+               \n\n **From Rj-** \n Senior Lars,
+               Happiest birthday :crown: :tada:
+               May all you career plans come true :man_scientist: and May all your credit cards get heavily loaded :dollar:. You have helped so many people in so many ways, You are one of the best people I have came to know in person.
+               Senior Lars, the 4chambers of my heart really grateful to know you.Thanks for all the advices regarding The PhD interviews.Thanks for making so many amazing bots and adding new features always.Thank you for making this amazing server that gave me a chance to meet so many good friends.MOST IMPORTANTLY THANK YOU FOR BEING WHO YOU ARE, BCZ YOU ARE AWESOME. Even if you are 14yr kid or 28yr man,you are best at both.
+               Wish you all the happiness of the world.May all of your birthday wishes come true.
+               Happy birthday Senior
+               With lots of gratitude,love,wishes and prayers
+               rj
+               \n\n **From Jerry-** \n Ciao Lars, i wish you happy birthday and thousands of these happy days. Thanks for the effort you put everyday on the server, still, enjoy your birthday! :cake: :penguin:
+               \n\n **From JayJ-**  \n Here is your wishing message
+               Dear Lars, I am still used to Robin :joy:,
+               HAPPY BIRHTDAY!!! :partying_face::partying_face: :g_mmPartyCelebrateMocha: :balloon: :champagne:
+               You are seriously one of the most self-less, understanding, kind person I've ever met,
+               What you've done for this server is create millions of smiles and great moments that all of us will never forget in our lives!
+               You deserve the all of that joy back to you, and anything I can do or anyone can do for you let me or us know!
+               I'm glad to call you friend and co-mod (or boss :p)
+               I will sing you happy b-day anyday anytime so you remember my lovely singing :joy:
+               - Love, your friend JJay
+               \n\n **From Berra-** \n Happyyy Birthdayy Larss!!!:partying_face: :partying_face: :partying_face: Thank you for all the things you have done for us,for the server.This server helped me a lot about so many things in my life and Im SOOO GRATEFUL for you for making this real,always helping us whenever we need,putting soooo much effort into this.You are really kind,funny and a wonderful person.I hope you achieve all of your goals and dreams this year.Sending lots of love and positive energy. All the bestt in your new age :purple_heart: :purple_heart:
+               \n\n **From Aya-** \n Robin/Lars/rap aspirant/Dutch dad, many hats and names and one "pure" soul yet so many talents!
+               Thank you for everything you do for us on the server, what a home you guys have built!
+               I wish you a very happy birthday, to make it through one more year as gracefully as you can :sparkles:
+               - Mama Aya
+               \n\n **From Yigit-** \n Happy birthday Larsssss! :partying_face::tada::partying_face::tada::partying_face::tada: Hey man hope you have a wonderful day. It's really awesome to know someone like you. You made a great change in our lives, thanks for everything you've done so far. You deserve the very very best. Stay tranquilo 
+               \n\n **From Rob-** \n Happy birthday!!! :partying_face: Have the best new year, and from the bottom of my heart thanks for everything you do! Stay awesome and keep up the good work!
+               \n\n **From Bibi-** \n Happy birthday Robin, Lars, bobin x) :partying_face: ! You are such a good soul and I hope you all the happiness of the world. Thanks for helping this server so much - you are always listening to our weird suggestion to make the server a better place . Take care of you and your family - You deserve all the best for everything in your life . Stay awesome :3 :white_heart: :cake:
+                 ` },       
+                 
+                 client.users.fetch('763285109280604180').then(user => {
+                    user.send(BdayEmbed)
+                  } )
+          )
+                },
+                permissions: ['ADMINISTRATOR'],
+                requiredRoles: []
+        
+   
+}
