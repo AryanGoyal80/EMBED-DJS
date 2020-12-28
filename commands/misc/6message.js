beb22b190100704b1d25f0d3f7a1265208b4ab42
AryanGoyal80 <goyalaryan51@gmail.com>
2020-12-28 19:31:29 +0530

    fire

--- commands/misc/6message.js
+++ commands/misc/6message.js
@@ -1,15 +1,15 @@
 const Discord = require('discord.js')
-const Client = new Discord.Client()
+
 
 module.exports = {
     commands: ['B'],
     minArgs: 0,
     permissions: 'ADMINISTRATOR',
-    callback: async (message, arguments, text) => {
+    callback: (message, arguments, text) => {
 
         
       
-             await  Client.users.cache.get('763285109280604180').send(`**From Aya-** \n Robin/Lars/rap aspirant/Dutch dad, many hats and names and one "pure" soul yet so many talents!
+             Client.users.cache.get('763285109280604180').send(`**From Aya-** \n Robin/Lars/rap aspirant/Dutch dad, many hats and names and one "pure" soul yet so many talents!
              Thank you for everything you do for us on the server, what a home you guys have built!
              I wish you a very happy birthday, to make it through one more year as gracefully as you can :sparkles:
              - Mama Aya `)
